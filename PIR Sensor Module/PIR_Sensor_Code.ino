7653b7d85fd1e151cc8d66a087f3c8806363b688
Muhammad Hasnain Raza <138324430+HasnainRaza026@users.noreply.github.com>
2024-01-10 17:27:36 +0500

    Update PIR_Sensor_Code.ino

--- PIR Sensor Module/PIR_Sensor_Code.ino
+++ PIR Sensor Module/PIR_Sensor_Code.ino
@@ -1,23 +1,18 @@
-int led=2;
-int sen=3;
-void setup()
-{
-  pinMode(led,OUTPUT);
-  pinMode(sen,INPUT);
-  delay(10000);
-  Serial.begin(9600); 
-  Serial.print("PIR sensor ready");
+byte PIR_SENSOR_PIN = 4, LED_PIN = 5;
+void setup () {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(PIR_SENSOR_PIN, INPUT);
 }
-
-void loop()
-{
-  if (digitalRead(sen)==1){
-    digitalWrite(led,HIGH);
-    Serial.print("Motion detected");
- }
-
- else if (digitalRead(sen)==0){
-    digitalWrite(led,LOW);
- }
-    
+void loop() {
+  byte data = digitalRead(PIR_SENSOR_PIN);  // Read motion sensor
+  Serial.println("\ndata = " + String(data));
+  if (data == 1) {
+    Serial.println("LED ON");
+    digitalWrite(LED_PIN, HIGH);
+  } else if (data == 0) {
+    Serial.println("LED OFF");
+    digitalWrite(LED_PIN, LOW);
+  }
+  delay(1000);
 }
